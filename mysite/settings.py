a0c8255c2f21eee33a94324f76017bcdfd3c7f2b
sajjad0057 <sajjadhossain0057@gmail.com>
2021-08-12 11:49:43 +0600

    part 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #installed app
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
