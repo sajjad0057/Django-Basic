a68b9f6aab454fcdee5ea60d8274a07a940c1109
sajjad0057 <sajjadhossain0057@gmail.com>
2021-08-13 16:35:27 +0600

    Avoiding race conditions using F()

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
-from django import template
+from django.db.models import F
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question,Choice
 #from django.http import Http404
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 
 
@@ -26,10 +28,23 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question  = get_object_or_404(Question,pk=question_id)
+
+    try:
+        s_c = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question':question,
+            'error_message':"you didn't select a Choice .",
+        })
+    else:
+        s_c.votes = F('votes') + 1 
+        s_c.save()
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
