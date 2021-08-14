974b9a23fc3d86e334edae045a7e3f07545c426c
sajjad0057 <sajjadhossain0057@gmail.com>
2021-08-14 16:40:04 +0600

    customized django default admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,31 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['published_date','question_text']
+    fieldsets = [
+        ('Question', { 'fields':['question_text'] }),
+        ( 'Date Information', { 'fields':['published_date'],'classes':['collapse'] } ),
+    ]
+
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'published_date', 'was_published_recently')
+
+    list_filter = ['published_date']
+
+    search_fields = ['question_text']
+
+
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
